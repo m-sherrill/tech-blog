e95ee86849a5e29c0d0a5a9fe2b7e09d077e9097
Morgan Sherrill <mo.sherrill@outlook.com>
2021-12-22 13:04:56 -0800

    started trying to pull together login stuff -- added some boilerplate for reference, but need to modify and change it a lot to meet my req.body requirements

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,82 @@
-
 const router = require('express').Router();
+const { Comment, User, Blog } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-    // TODO: Render template with Sequelize data
-    res.render('homepage');
-  });
-  
-  module.exports = router;
+  try {
+    // Get all projects and JOIN with user data
+    const blogData = await Blog.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['display_name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const blogs = blogData.map((blog) => blog.get({ plain: true }));
+console.log(blogs)
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      ...blogs, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/blogs/:id', async (req, res) => {
+  try {
+    const blogData = await Blog.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['display_name'],
+        },
+      ],
+    });
+
+    const blogs = blogData.get({ plain: true });
+
+    res.render('blog', {
+      ...blogs,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Blog, Comment }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
--- seeds/index.js
+++ seeds/index.js
@@ -8,7 +8,6 @@ const sequelize = require('../config/connections');
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
-  
   await seedUser();
   console.log('\n----- USERS SEEDED -----\n');
 
@@ -16,7 +15,7 @@ const seedAll = async () => {
   console.log('\n----- BLOG POSTS SEEDED -----\n');
 
   await seedComment();
-  console.log('\n----- COMMENTS SEEDED -----\n');
+  console.log('\n----- TAGS SEEDED -----\n');
 
   process.exit(0);
 };
