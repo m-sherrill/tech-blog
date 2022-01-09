8f5973df69b3dda62efc747e6178d30195c73072
Morgan Sherrill <mo.sherrill@outlook.com>
2022-01-09 02:22:57 -0800

    heroku errors still

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Blog, Comment } = require('../../models');s
+const { User, Blog, Comment } = require('../models')
 const withAuth = require('../utils/auth');
 
 
