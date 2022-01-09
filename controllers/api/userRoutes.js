4290088381bc1e407e8d40bde7ed984e8056fb86
Morgan Sherrill <mo.sherrill@outlook.com>
2022-01-09 02:17:27 -0800

    ---

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Blog, Comment } = require('../../models');
+const { user, blog, comment } = require('../../models');
 
 
 router.get('/', async (req, res) => {
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Blog, Comment } = require('../../models');
+const { user, blog, comment } = require('../../models');
 const withAuth = require('../utils/auth');
 
 
